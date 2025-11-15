374dc605978498f59c837741556af09ce60a368f
Monojit Saha <monojitsaha@Monojits-MacBook-Pro.local>
2025-11-15 22:52:56 +0530

    multi-tier navigation

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import MarketingApp from "./components/MarketingApp";
+import Headers from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
-export default () => {
+const generateClassName = createGenerateClassName({ productionPrefix: "co" });
+
+function App() {
   return (
-    <div>
-      <h1>Hi there!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <StylesProvider generateClassName={generateClassName}>
+          <Headers />
+          <MarketingApp />
+        </StylesProvider>
+      </div>
+    </BrowserRouter>
   );
-};
+}
+
+export default App;
