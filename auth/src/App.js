844420f46a0a022b76f8d32c0e7b1613c2dc84b9
Monojit Saha <monojitsaha@Monojits-MacBook-Pro.local>
2025-11-20 22:27:40 +0530

    AUTH - complete

---
+++ auth/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Switch, Route, Router } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "au",
+});
+
+function App({ history, onSignIn }) {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/auth/signin">
+              <Signin onSignIn={onSignIn} />
+            </Route>
+            <Route path="/auth/signup">
+              <Signup onSignIn={onSignIn} />
+            </Route>
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+}
+
+export default App;
--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,27 +1,27 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8081/',
+  },
   devServer: {
-    port: 8080,
+    port: 8081,
     historyApiFallback: {
-      index: '/index.html',
+      historyApiFallback: true,
     },
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+        marketing: 'marketing@http://localhost:8082/remoteEntry.js',
+        auth: 'auth@http://localhost:8083/remoteEntry.js',
       },
-      shared: packageJson.dependencies
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
+      shared: packageJson.dependencies,
     }),
   ],
 };
